1d4be7c9aa6e83c552b671198409860433ec71be
Abderrahim-issou <155481664+Abderrahim-issou@users.noreply.github.com>
2024-12-04 12:14:11 +0100

    new pro

---
+++ Mypoject/test.php
@@ -0,0 +1,37 @@
+<?php
+
+echo 'hello';
+echo 'hello';
+echo 'hello';
+echo 'hello';
+echo 'hello';
+echo 'hello';
+echo 'hello';
+echo 'hello';
+echo 'hello';
+echo 'hello';
+echo 'hello';
+echo 'hello';
+echo 'hello';
+echo 'hello';
+echo 'hello';
+echo 'hello';
+echo 'hello';
+echo 'hello';
+echo 'hello';
+echo 'hello';
+echo 'hello';
+echo 'hello';
+echo 'hello';
+
+
+$a=74;
+$a=74;
+$a=74;
+$a=74;
+$a=74;
+$a=74;
+
+?>
+
+
